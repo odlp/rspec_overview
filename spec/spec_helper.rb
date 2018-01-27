c3341ac9f884d153369c8bd90877cfef77cc88d6
Oli Peate <oliverp@gmail.com>
2018-01-27 19:37:19 +0000

    Verify doubles & run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,17 @@ require "bundler/setup"
 require "rspec_overview"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
 end
