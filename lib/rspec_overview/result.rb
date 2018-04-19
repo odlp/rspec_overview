d782c6476156fffc668761b0f92dae86cd1c8ce4
Oli Peate <oli@thoughtbot.com>
2018-04-19 17:52:57 +0200

    Correctly namespace Result

--- lib/rspec_overview/result.rb
+++ lib/rspec_overview/result.rb
@@ -1,24 +1,26 @@
-class Result
-  attr_reader :identifier
-  attr_accessor :example_count, :duration_raw
+module RspecOverview
+  class Result
+    attr_reader :identifier
+    attr_accessor :example_count, :duration_raw
 
-  def initialize(identifier)
-    @identifier = identifier
-    @example_count = 0
-    @duration_raw = 0.0
-  end
+    def initialize(identifier)
+      @identifier = identifier
+      @example_count = 0
+      @duration_raw = 0.0
+    end
 
-  def avg_duration_seconds
-    format_seconds(duration_raw / example_count)
-  end
+    def avg_duration_seconds
+      format_seconds(duration_raw / example_count)
+    end
 
-  def duration_seconds
-    format_seconds(duration_raw)
-  end
+    def duration_seconds
+      format_seconds(duration_raw)
+    end
 
-  private
+    private
 
-  def format_seconds(duration)
-    RSpec::Core::Formatters::Helpers.format_seconds(duration)
+    def format_seconds(duration)
+      RSpec::Core::Formatters::Helpers.format_seconds(duration)
+    end
   end
 end
